fa760565b0989290e2478ff0d68f51ba6f0d64ce
xsynch <jdstrickland@gmail.com>
2025-09-28 12:57:16 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xsynch/scrapetojson
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("This will work to take scraping data and return json to client")
+}
