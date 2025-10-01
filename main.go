6297850cfacf07a942872c3f8f09527763d83aa9
xsynch <jdstrickland@gmail.com>
2025-09-30 21:47:42 -0400

    first webscrape attempt

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main(){
 	fmt.Println("This will work to take scraping data and return json to client")
+	collector := colly.NewCollector(colly.AllowedDomains("www.scrapingcourse.com"),)
+
+
+	
+
+	collector.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting: ", r.URL)
+    })
+
+	collector.OnResponse(func(r *colly.Response){
+		fmt.Println("Page Visited: ", r.Request.URL )
+	})
+
+	collector.Visit("https://www.scrapingcourse.com/ecommerce")
 }
